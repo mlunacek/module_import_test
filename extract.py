f24a042781dd70acca67a9e133e4a5985b4017e6
Monte Lunacek <monte.lunacek@nrel.gov>
2022-02-03 13:48:23 -0700

    example

---
+++ extract.py
@@ -0,0 +1,12 @@
+
+from validate import func3
+
+def main():
+
+    print("extract.py")
+    func3()
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ validate.py
@@ -0,0 +1,14 @@
+
+
+def func1():
+    print("func1")
+
+
+def func2():
+    print("func2")
+
+def func3():
+    func1()
+    func2()
+
+    
